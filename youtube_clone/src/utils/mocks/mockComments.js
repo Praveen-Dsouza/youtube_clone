9348e7821221633bacdb431a3201187d97027265
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-12-06 20:36:35 +0530

    added n level nested dummy comments

---
+++ youtube_clone/src/utils/mocks/mockComments.js
@@ -0,0 +1,233 @@
+export const commentsData = [
+    {
+        name: 'Jon Doe',
+        text: 'This is my first comment',
+        replies: []
+    },
+    {
+        name: 'Jon Doe',
+        text: 'This is my first comment',
+        replies: [
+            {
+                name: 'Jon Doe',
+                text: 'This is my first comment',
+                replies: []
+            },
+            {
+                name: 'Jon Doe',
+                text: 'This is my first comment',
+                replies: [
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: []
+                    }
+                ]
+            },
+            {
+                name: 'Jon Doe',
+                text: 'This is my first comment',
+                replies: [
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: [
+                            {
+                                name: 'Jon Doe',
+                                text: 'This is my first comment',
+                                replies: [
+                                    {
+                                        name: 'Jon Doe',
+                                        text: 'This is my first comment',
+                                        replies: [
+                                            {
+                                                name: 'Jon Doe',
+                                                text: 'This is my first comment',
+                                                replies: []
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Jon Doe',
+        text: 'This is my first comment',
+        replies: [
+            {
+                name: 'Jon Doe',
+                text: 'This is my first comment',
+                replies: [
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: [
+                            {
+                                name: 'Jon Doe',
+                                text: 'This is my first comment',
+                                replies: []
+                            },
+                            {
+                                name: 'Jon Doe',
+                                text: 'This is my first comment',
+                                replies: [
+                                    {
+                                        name: 'Jon Doe',
+                                        text: 'This is my first comment',
+                                        replies: [
+                                            {
+                                                name: 'Jon Doe',
+                                                text: 'This is my first comment',
+                                                replies: [
+                                                    {
+                                                        name: 'Jon Doe',
+                                                        text: 'This is my first comment',
+                                                        replies: []
+                                                    },
+                                                    {
+                                                        name: 'Jon Doe',
+                                                        text: 'This is my first comment',
+                                                        replies: [
+                                                            {
+                                                                name: 'Jon Doe',
+                                                                text: 'This is my first comment',
+                                                                replies: []
+                                                            }
+                                                        ]
+                                                    },
+                                                    {
+                                                        name: 'Jon Doe',
+                                                        text: 'This is my first comment',
+                                                        replies: [
+                                                            {
+                                                                name: 'Jon Doe',
+                                                                text: 'This is my first comment',
+                                                                replies: [
+                                                                    {
+                                                                        name: 'Jon Doe',
+                                                                        text: 'This is my first comment',
+                                                                        replies: [
+                                                                            {
+                                                                                name: 'Jon Doe',
+                                                                                text: 'This is my first comment',
+                                                                                replies: [
+                                                                                    {
+                                                                                        name: 'Jon Doe',
+                                                                                        text: 'This is my first comment',
+                                                                                        replies: []
+                                                                                    }
+                                                                                ]
+                                                                            }
+                                                                        ]
+                                                                    }
+                                                                ]
+                                                            },
+                                                            {
+                                                                name: 'Jon Doe',
+                                                                text: 'This is my first comment',
+                                                                replies: []
+                                                            }
+                                                        ]
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                name: 'Jon Doe',
+                                text: 'This is my first comment',
+                                replies: [
+                                    {
+                                        name: 'Jon Doe',
+                                        text: 'This is my first comment',
+                                        replies: [
+                                            {
+                                                name: 'Jon Doe',
+                                                text: 'This is my first comment',
+                                                replies: [
+                                                    {
+                                                        name: 'Jon Doe',
+                                                        text: 'This is my first comment',
+                                                        replies: [
+                                                            {
+                                                                name: 'Jon Doe',
+                                                                text: 'This is my first comment',
+                                                                replies: []
+                                                            }
+                                                        ]
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        name: 'Jon Doe',
+                                        text: 'This is my first comment',
+                                        replies: []
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'Jon Doe',
+                text: 'This is my first comment',
+                replies: [
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: []
+                    }
+                ]
+            },
+            {
+                name: 'Jon Doe',
+                text: 'This is my first comment',
+                replies: [
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: [
+                            {
+                                name: 'Jon Doe',
+                                text: 'This is my first comment',
+                                replies: [
+                                    {
+                                        name: 'Jon Doe',
+                                        text: 'This is my first comment',
+                                        replies: [
+                                            {
+                                                name: 'Jon Doe',
+                                                text: 'This is my first comment',
+                                                replies: []
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Jon Doe',
+                        text: 'This is my first comment',
+                        replies: []
+                    }
+                ]
+            }
+        ]
+    }
+]
