28ebeb3ca3674fcd320104ce8fe40a11250b9cbe
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-12-13 16:58:54 +0530

    fixed Shimmer

--- youtube_clone/src/components/Shimmer.js
+++ youtube_clone/src/components/Shimmer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const VideoCard = () => {
   return (
-    <div className="p-2 m-2 w-80 h-[250px]">
-      <div className="w-full h-44 rounded-xl bg-slate-300"></div>
+    <div className="p-2 m-2 w-96 h-80">
+      <div className="w-full h-56 rounded-xl bg-slate-300"></div>
       <div className="pt-3 flex">
         <div className="w-10 h-10 rounded-full bg-slate-300"></div>
         <div className="w-full mx-2">
@@ -17,8 +17,8 @@ const VideoCard = () => {
 
 const Shimmer = () => {
   return (
-    <div className="flex flex-wrap justify-center px-10 w-full">
-      {Array(20)
+    <div className="flex flex-wrap justify-center w-full">
+      {Array(12)
         .fill()
         .map((index) => {
           return <VideoCard key={index} />;
