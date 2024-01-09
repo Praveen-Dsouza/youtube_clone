d5c341a090f08554f32a27b3ef9cd95a31e1d965
Praveen Dsouza <dsouzapraveen24@gmail.com>
2024-01-09 17:41:22 +0530

    fixed buttonlist fade css

--- youtube_clone/src/components/main/ButtonList.js
+++ youtube_clone/src/components/main/ButtonList.js
@@ -27,15 +27,17 @@ const ButtonList = () => {
 
   return (
     <div className="flex bg-white fixed w-[calc(96%)] overflow-hidden">
-      <button onClick={() => scrollBtns("prev")} className={`absolute py-5 bg-gradient-to-r from-white ${hideBtns === 0 && 'hidden'}`}>
-        <div className="hover:bg-gray-200 bg-white p-1 rounded-full">
-          <img
-            className="h-4 w-4 cursor-pointer"
-            src={backArrow}
-            alt=""
-          />
-        </div>
-      </button>
+      <div className={`w-14 bg-gradient-to-r from-white ${hideBtns === 0 && 'hidden'}`}>
+        <button onClick={() => scrollBtns("prev")} className={`absolute py-5`}>
+          <div className="hover:bg-gray-200 bg-white p-1 rounded-full">
+            <img
+              className="h-4 w-4 cursor-pointer"
+              src={backArrow}
+              alt=""
+            />
+          </div>
+        </button>
+      </div>
       <div ref={btnContainerRef} className="flex overflow-x-hidden mx-2">
         {BUTTON_NAMES.map((item) => (
           <Link key={item} to={"/results?search_query=" + item}>
@@ -43,15 +45,17 @@ const ButtonList = () => {
           </Link>
         ))}
       </div>
-      <button onClick={() => scrollBtns("next")} className={`absolute right-0 py-5 bg-gradient-to-l from-white ${hideBtns >= 230 && 'hidden'}`}>
-        <div className="hover:bg-gray-200 bg-white p-1 rounded-full">
-          <img
-            className="h-4 w-4 cursor-pointer"
-            src={forwardArrow}
-            alt=""
-          />
-        </div>
-      </button>
+      <div className={`w-14 bg-gradient from-white ${hideBtns >= 200 && 'hidden'}`}>
+        <button onClick={() => scrollBtns("next")} className={`absolute right-0 py-5`}>
+          <div className="hover:bg-gray-200 bg-white p-1 rounded-full">
+            <img
+              className="h-4 w-4 cursor-pointer"
+              src={forwardArrow}
+              alt=""
+            />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
